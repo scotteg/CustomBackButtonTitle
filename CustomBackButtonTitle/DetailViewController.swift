796107b29448ca88fd83fe7959c7e00e6fafa6e6
Scott Gardner <scott.gardner@mac.com>
2014-11-19 11:33:54 -0600

    Set back button programmatically

--- CustomBackButtonTitle/DetailViewController.swift
+++ CustomBackButtonTitle/DetailViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class DetailViewController: UIViewController {
 
   @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+  
   var detailItem: AnyObject? {
     didSet {
         // Update the view.
