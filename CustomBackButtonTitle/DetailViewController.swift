31c346f519676a9f44b99a78c781044b9c3dfe40
Scott Gardner <scott.gardner@mac.com>
2014-11-19 11:23:28 -0600

    Initial commit

---
+++ CustomBackButtonTitle/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  CustomBackButtonTitle
+//
+//  Created by Scott Gardner on 11/19/14.
+//  Copyright (c) 2014 Scott Gardner. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  var detailItem: AnyObject? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail: AnyObject = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
